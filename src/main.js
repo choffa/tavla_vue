c464eff7db320d1002ab3aac4ef63550116903e8
choffa <cgjovag@gmail.com>
2019-10-29 15:31:27 +0100

    Add Vue-Apollo

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { createProvider } from './vue-apollo'
 
 library.add(faBus)
- 
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
