aadf65e082431daffadc0b90e228897c4bfd7e8f
choffa <cgjovag@gmail.com>
2020-02-23 17:19:59 +0100

    Add header container

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="container">
+    <h1>TEST TEST TEST</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  height: 4em;
+  width: 100vw;
+  position: sticky;
+  top: 0;
+  right: 0;
+  box-shadow: 0px 2px 4px grey;
+  align-items: center;
+  justify-content: center;
+}
+</style>
