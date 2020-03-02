c1abf2d14ae5573b64ec8c512c90206bf8254b69
choffa <cgjovag@gmail.com>
2020-03-02 15:32:05 +0100

    Remove scrollbars outside Chrome also

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,12 +11,13 @@
   min-width: 20em;
   height: 25em;
   padding: 8px;
-  box-sizing: border-box;
   box-shadow: 1px 1px 2px grey;
   overflow-y: scroll;
   margin-right: 8px;
   margin-bottom: 8px;
   scrollbar-width: none;
+  -ms-overflow-style: none;
+  overflow: -moz-scrollbars-none;
 }
 
 .card::-webkit-scrollbar {
